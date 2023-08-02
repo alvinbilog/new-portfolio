f9a78fbe71f5114c09c39eabdd798cae39de51c5
Alvin Patrick Bilog <balvinpatrick@yahoo.com>
2023-08-01 19:11:43 -0500

    responsive home component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,7 +13,22 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        orange: {
+          fade: '#eb8c04',
+          strong: '#f2940c',
+          light: '#f7d4a3',
+          mild: '#eb8c0a',
+        },
+        text: {
+          normal: '#525252',
+          orange: '#eb8c0a',
+        },
+      },
     },
+    // dropShadow: {
+    //   '3xl': '35px 35px 35px rgba(247,212,163)',
+    // },
   },
   plugins: [],
-}
+};
